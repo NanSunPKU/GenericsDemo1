dab141a4af65c1a38cbb34ceeb192f0b285fbbf8
NanSun1963 <33167636+NanSunPKU@users.noreply.github.com>
2020-08-20 15:15:53 -0500

    Added CollectionsDemo

---
+++ src/com/company/collections/CollectionsDemo.java
@@ -0,0 +1,13 @@
+package com.company.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class CollectionsDemo {
+    public static void show() {
+        Collection<String> collection = new ArrayList<>();
+        Collections.addAll(collection, "a", "b");
+        System.out.println(collection.size());
+    }
+}
